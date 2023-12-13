3f7320477bf8ce7084e6abb0add028c93fc3695e
Nicolas Aldunate <naldunate@alumnosuls.cl>
2023-12-13 20:18:00 -0300

    asasdasdasdasdasd

--- app/users/usermodel.py
+++ app/users/usermodel.py
@@ -1,7 +1,7 @@
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, sqlite_autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=True)
